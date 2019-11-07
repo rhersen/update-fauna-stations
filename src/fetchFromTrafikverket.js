b10b44ce43713eb09f7a8f7233e122aa72422c8a
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-11-07 22:05:36 +0100

    move code down to /src/

---
+++ src/fetchFromTrafikverket.js
@@ -0,0 +1,49 @@
+import _ from "lodash"
+import fetch from "node-fetch"
+import yenv from "yenv"
+const env = yenv()
+
+export default async () => {
+  try {
+    const response = await fetch(
+      "http://api.trafikinfo.trafikverket.se/v1.2/data.json",
+      {
+        method: "POST",
+        body: getBody(),
+        headers: {
+          "Content-Type": "application/xml",
+          Accept: "application/json"
+        }
+      }
+    )
+    if (!response.ok) {
+      console.log(response.statusText)
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ msg: response.statusText })
+      }
+    }
+
+    const data = await response.json()
+    const [body] = data.RESPONSE.RESULT
+    console.log("api  ", _.max(_.map(body.TrainStation, "ModifiedTime")))
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(body)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+function getBody() {
+  return `
+<REQUEST>
+    <LOGIN authenticationkey='${env.TRAFIKVERKET}' />
+    <QUERY objecttype='TrainStation'>
+        <FILTER>
+        </FILTER>
+    </QUERY>
+</REQUEST>`
+}
